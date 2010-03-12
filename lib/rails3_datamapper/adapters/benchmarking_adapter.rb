1f53484c84394b5449f32e515d9b6004e63466d6
Quinn Shanahan <quinn.shanahan@controlgroup.com>
2010-03-12 16:04:40 -0500

    fixing for jruby

--- lib/rails3_datamapper/adapters/benchmarking_adapter.rb
+++ lib/rails3_datamapper/adapters/benchmarking_adapter.rb
@@ -1,37 +1,38 @@
 require 'rails3_datamapper/adapters/adapter_wrapper'
 
 module Rails
- module DataMapper
-   module Adapters
+  module DataMapper
+    module Adapters
 
-     class BenchmarkingAdapter
+      class BenchmarkingAdapter
+        include ::DataMapper::Adapters::Wrapper
 
-       include ::DataMapper::Adapters::Wrapper
+        def reset_runtime
+          rt, @runtime = @runtime, 0
+          rt
+        end
 
-       def reset_runtime
-         rt, @runtime = @runtime, 0
-         rt
-       end
+        %w(create read update delete).each do |method|
+          eval_string = "
+          def #{method} *args, &block
+            result = nil
+            @runtime += Benchmark.ms { 
+              result = adapter.send('#{method}', *args, &block) 
+            }
+            result
+          end"
+          eval(eval_string, binding, __FILE__, __LINE__)
+        end
 
-       %w(create read update delete).each do |method|
+        private
 
-         define_method method do |*args, &block|
-           result = nil
-           @runtime += Benchmark.ms { result = adapter.send(method, *args, &block) }
-           result
-         end
+        def initialize(adapter)
+          super
+          @runtime = 0
+        end
 
-       end
+      end
 
-       private
-
-       def initialize(adapter)
-         super
-         @runtime = 0
-       end
-
-     end
-
-   end
- end
+    end
+  end
 end
